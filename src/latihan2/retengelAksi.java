eddcc6dc91e9852b220dca39f9d34e2ccb0c8c4a
aguskris <agusabad2000@gmail.com>
2018-03-31 12:23:19 +0700

    menghitung panjang dan rendah mendapatkan hasil jarak

--- src/latihan2/retengelAksi.java
+++ src/latihan2/retengelAksi.java
@@ -5,4 +5,15 @@ public class retengelAksi {
        retengel r1 = new retengel();
        r1.panjang = 8;
        r1.rendah = 6;
+       
+       r1.cetakInfo();
+       System.out.println("rendah retengel = "+r1.hitungjarak());
+       r1.cetakjarak();
+   
+       retengel r2= new retengel();
+       r2.cetakInfo();
+       
+       retengel r3= new retengel(50,30);
+       r3.cetakInfo();
+   } 
 }
