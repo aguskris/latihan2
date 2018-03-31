4ba1324d96ea8610ba3aa88b8e25a7ad27f55937
aguskris <agusabad2000@gmail.com>
2018-03-31 12:19:22 +0700

    memasukkan nilai

--- src/latihan2/retengelAksi.java
+++ src/latihan2/retengelAksi.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package latihan2;
 
-/**
- *
- * @author user
- */
 public class retengelAksi {
-    
+   public static void main(String[] args){
+       retengel r1 = new retengel();
+       r1.panjang = 8;
+       r1.rendah = 6;
 }
